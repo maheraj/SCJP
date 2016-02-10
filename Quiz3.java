165038bca6f14403fc5c5eb372db3a024b641042
Maheraj Uddin <maheraj.uddin@ideascale.com>
2016-02-10 11:34:58 +0600

    Create Quiz3.java

---
+++ Quiz3.java
@@ -0,0 +1,17 @@
+public class Quiz3 {
+    public static String getValue(Integer value) {
+        return "integer";
+    }
+
+    public static String getValue(Object value) {
+        return "object";
+    }
+
+    public static void process() {
+        System.out.println(getValue(null));
+    }
+
+    public static void main(String[] args) {
+        process();
+    }
+}
